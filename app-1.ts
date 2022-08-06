8467087ef00727b9328045f4a260efeaae73afcd
Laiamdelprado <lmarco@userlytics.com>
2022-08-06 13:26:50 +0200

    use of switch in reducer

--- app-1.ts
+++ app-1.ts
@@ -12,11 +12,15 @@ const incrementAction: Action = {
 //HOW TO USE AN ACTION
 //REDUCER: is a simple function which recives two arguments: the current state and the action I want to implement. 
 //The objective of the reducer is to return a new state.
+//If we want to include other condition in the reduce we do not use 'if... else'. In this case, we use 'switch'.
 function reducer(state: 10, action: Action) {
-    if(action.type === 'Increment') {
+    switch(action.type) {
+        case 'Increment': 
         return state += 1;
+
+        default:
+            return state;
     }
-    return state;
 }
 
 //HOW TO USE A REDUCER
