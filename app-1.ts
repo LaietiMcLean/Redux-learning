ce05a63628547ebb08cd20aacd42be2bb60364a6
Laiamdelprado <lmarco@userlytics.com>
2022-08-06 13:53:00 +0200

    reducer actions and payload basic use

--- app-1.ts
+++ app-1.ts
@@ -19,10 +19,25 @@ const multiplyAction: Action = {
     type: 'Multiply'
 }
 
+//Fourth action
+const multiplyArgumentAction: Action = {
+    type: 'MultiplyArgument',
+    payload: 3
+}
+
+//Fifth action
+const divideArgumentAction: Action = {
+    type: 'DivideArgument',
+    payload: 2
+}
+
 //HOW TO USE AN ACTION
 //REDUCER: is a simple function which recives two arguments: the current state and the action I want to implement. 
 //The objective of the reducer is to return a new state.
 //If we want to include other condition in the reduce we do not use 'if... else'. In this case, we use 'switch'.
+//A reducer is a simple function that has a switch with every action the reducer can operate.
+/*Reducer must be a pure function; nothing has to interfere with it. 
+Everything the Reducer needs to resolve it will be done here, and never with outside actions (for example, there won't be HTTP requests)*/
 function reducer(state: 10, action: Action) {
     switch(action.type) {
         case 'Increment': 
@@ -35,6 +50,14 @@ function reducer(state: 10, action: Action) {
         //Third action:
         case 'Multiply':
             return state *=2;
+
+        //Fourth action
+        case 'MultiplyArgument':
+            return state * action.payload;
+        
+        //Fifth action
+        case 'DivideArgument':
+            return state / action.payload;
 
         default:
             return state;
@@ -49,3 +72,5 @@ reducer(10, multiplyAction);
 console.log(reducer(10, incrementAction)); // return: 11
 console.log(reducer(10, decrementAction)); // return: 9
 console.log(reducer(10, multiplyAction)); // return: 20
+console.log(reducer(10, multiplyArgumentAction)); // return: 30
+console.log(reducer(10, divideArgumentAction)); // return: 5
